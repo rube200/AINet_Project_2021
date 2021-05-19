c4d8c7363b968cb52648a9ccd2b10c51b4bae2aa
Rúben Garcia <rube200@users.noreply.github.com>
2021-05-19 23:30:14 +0100

    Dashboard suporte para estampas privadas

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,39 +3,41 @@
 namespace App\Http\Controllers;
 
 use App\Models\Estampa;
-use http\Env\Request;
-use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class DashboardController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //$this->middleware(['auth', 'verified']);
-    }
-
-    /**
      * Show the application dashboard.
      *
      * @param Request $request
-     * @return Renderable
+     * @return mixed
      */
-    public function index(Request $request): Renderable
+    public function index(Request $request)
     {
-        if (!$request->has('categoria') || ($categoria = $request->query('categoria', '0')) == 0)
-        {
-            $listaEstampas = Estampa::pluck('id', 'nome', 'descricao', 'imagem_url');
+        $estampasQuery = Estampa::select('nome', 'descricao', 'imagem_url')
+            ->whereNull('cliente_id');
+        if (($categoria = $request->query('categoria', '0')) != 0)//todo verificar query
+            $estampasQuery->where('categoria_id', $categoria);
+        $estampas = $estampasQuery->get();
+
+        foreach ($estampas as $estampa)
+            $estampa->img = asset('storage/estampas/' . $estampa->imagem_url);
+
+        if (Auth::check()) {
+            $estampasPrivadas = Estampa::select('nome', 'descricao', 'imagem_url')
+                ->where('cliente_id', Auth::id())
+                ->get();
+
+            foreach ($estampasPrivadas as $estampa)
+                $estampa->img = 'data:image/png;base64,' . base64_encode(Storage::get('estampas_privadas/' . $estampa->imagem_url));
+
+            $estampas = $estampas->toBase()->merge($estampasPrivadas);
         }
-        else
-        {
-            $listaEstampas = Estampa::where('categoria_id', $categoria)
-                ->pluck('id', 'nome', 'descricao', 'imagem_url');
-        }
+
         return view('dashboard.index')
-            ->withEstampas($listaEstampas);
+            ->withEstampas($estampas);
     }
 }
