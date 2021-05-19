1218eb8d5cd6afbbaf6f3e67c3a129433cec4dd8
Rúben Garcia <rube200@users.noreply.github.com>
2021-05-19 23:30:31 +0100

    Model Estampa Metadados

--- app/Models/Estampa.php
+++ app/Models/Estampa.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @method static where(string $string, array|string|null $categoria)
+ * @method static select(string $string, string $string1, string $string2)
+ */
 class Estampa extends Model
 {
     use HasFactory, SoftDeletes;
