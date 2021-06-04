12f6a28e9d4a9588aae358ada7c780fb6b14136d
Rúben Garcia <rube200@users.noreply.github.com>
2021-06-04 17:47:00 +0100

    Adding single estampa view

--- resources/views/partials/display_estampas.blade.php
+++ resources/views/partials/display_estampas.blade.php
@@ -2,12 +2,13 @@
     @foreach($estampas as $estampa)
         <div class="col-lg-3">
             <div class="card estampa-card">
-                <img class="card-img-top estampa-img" src="{{$estampa->img}}" alt="Imagem da estampa">
+                <a class="estampa-item-link" href="{{route('estampa.view', $estampa->id)}}">
+                    <img class="card-img-top estampa-img" src="{{$estampa->img}}" alt="Imagem da estampa">
                 <div class="card-body estampa-body">
                     <h5 class="card-title estampa-name">{{$estampa->nome}}</h5>
                     <p class="card-text estampa-descricao">{{$estampa->descricao}}</p>
                     <form action="{{route('cart.add')}}" method="POST">
-                        {{csrf_field()}}
+                        @csrf
                         <input type="hidden" value="{{$estampa->id}}" id="estampa-id" name="estampa-id">
                         <div class="row">
                             <button class="btn btn-secondary btn-sm">
@@ -16,6 +17,7 @@
                         </div>
                     </form>
                 </div>
+                </a>
             </div>
         </div>
     @endforeach
