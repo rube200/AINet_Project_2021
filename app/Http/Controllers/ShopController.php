3eba0cd47fd4055b74dd393734bbec974956d3cb
Rúben Garcia <rube200@users.noreply.github.com>
2021-06-04 17:46:35 +0100

    Update ShopController.php

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -17,26 +17,33 @@ class ShopController extends Controller
      */
     public function index(Request $request)
     {
-        $estampasQuery = Estampa::select('nome', 'descricao', 'imagem_url')
-            ->whereNull('cliente_id');
+        $estampasQuery = Estampa::select('id', 'nome', 'descricao', 'imagem_url')->whereNull('cliente_id');
         if (($categoria = $request->query('categoria', '0')) != 0)//todo verificar query
+        {
             $estampasQuery->where('categoria_id', $categoria);
+        }
+
         $estampas = $estampasQuery->get();
-
         foreach ($estampas as $estampa)
+        {
             $estampa->img = asset('storage/estampas/' . $estampa->imagem_url);
+        }
 
-        if (Auth::check()) {
-            $estampasPrivadas = Estampa::select('nome', 'descricao', 'imagem_url')
-                ->where('cliente_id', Auth::id())
-                ->get();
-
+        if (Auth::check())
+        {
+            $estampasPrivadas = Estampa::select('id', 'nome', 'descricao', 'imagem_url')->where('cliente_id', Auth::id())->get();
             foreach ($estampasPrivadas as $estampa)
+            {
                 $estampa->img = 'data:image/png;base64,' . base64_encode(Storage::get('estampas_privadas/' . $estampa->imagem_url));
-
+            }
             $estampas = $estampas->toBase()->merge($estampasPrivadas);
         }
 
         return view('shop.index')->withEstampas($estampas);
+    }
+
+    public function show(Estampa $estampa)
+    {
+        return view('shop.estampa')->withEstampa($estampa);
     }
 }
