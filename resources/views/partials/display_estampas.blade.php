50259cd128141ee9decb8ad83d8984cab97665d4
Rúben Garcia <rube200@users.noreply.github.com>
2021-06-04 13:51:43 +0100

    Update layout and display estampas | create navbar

--- resources/views/partials/display_estampas.blade.php
+++ resources/views/partials/display_estampas.blade.php
@@ -1,12 +1,21 @@
-<div class="estampa-area">
+<div class="row estampa-area">
     @foreach($estampas as $estampa)
-        <div class="estampa">
-            <div class="estampa-imagem">
-                <img src="{{$estampa->img}}" alt="Imagem da estampa">
-            </div>
-            <div class="estampa-info-area">
-                <div class="estampa-name">{{$estampa->nome}}</div>
-                <div class="estampa-descricao">{{$estampa->descricao}}</div>
+        <div class="col-lg-3">
+            <div class="card estampa-card">
+                <img class="card-img-top estampa-img" src="{{$estampa->img}}" alt="Imagem da estampa">
+                <div class="card-body estampa-body">
+                    <h5 class="card-title estampa-name">{{$estampa->nome}}</h5>
+                    <p class="card-text estampa-descricao">{{$estampa->descricao}}</p>
+                    <form action="{{route('cart.add')}}" method="POST">
+                        {{csrf_field()}}
+                        <input type="hidden" value="{{$estampa->id}}" id="estampa-id" name="estampa-id">
+                        <div class="row">
+                            <button class="btn btn-secondary btn-sm">
+                                Add to cart
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     @endforeach
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,7 @@
+<nav class="bg-dark fixed-top navbar navbar-dark shadow-lg">
+    <div class="container">
+        <a class="navbar-brand" href="{{route('index')}}">
+            {{config('app.name', 'MagicShirts')}}
+        </a>
+    </div>
+</nav>
