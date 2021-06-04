7786f6c32599a4b985efb0af57de45b5b6f0cb5e
Rúben Garcia <rube200@users.noreply.github.com>
2021-06-04 17:45:00 +0100

    Create CartController.php

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    public function index()
+    {
+        $cart = session()->get('cart');
+        if (!$cart) {
+            $cart = [];
+            session()->put('cart', $cart);
+        }
+        return view('cart.index');
+    }
+
+    public function add(Request $request)
+    {
+        //$id = $request->id;
+        if (false) {
+            abort(404);
+        }
+
+        $cart = session()->get('cart');
+        if (!$cart) {
+            $cart = [
+                /*$id => [
+                    "quantidade" => 1
+                ]*/
+            ];
+
+            session()->put('cart', $cart);
+            return redirect()->back();
+        }
+
+        return redirect()->back();
+
+        if (isset($cart[$id])) {
+            $cart[$id]['quantidade']++;
+
+            session()->put('cart', $cart);
+            return redirect()->back();
+        }
+
+        $cart[$id] = [
+            /*$id => [
+                "quantidade" => 1
+            ]*/
+        ];
+
+        session()->put('cart', $cart);
+        return redirect()->back();
+    }
+}
