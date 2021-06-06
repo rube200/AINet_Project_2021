8c2f8d5478624136a9a81d18a919a29b72f1e93a
Rúben Garcia <rube200@users.noreply.github.com>
2021-06-06 15:51:26 +0100

    Update estampa view

--- resources/views/shop/estampa.blade.php
+++ resources/views/shop/estampa.blade.php
@@ -1,41 +1,35 @@
-@extends('layouts.layout')
-@section('title', 'Shop')
+@extends('layouts.shop_layout')
+@section('title', __('Estampa-Title', ['name' => $estampa->nome]))
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md">
-                <img class="estampa-img" src="{{$estampa->img}}" alt="Imagem da estampa">
+                <img class="estampa-img" src="{{$estampa->img}}">
             </div>
             <div class="col-md">
                 <div class="card-body estampa-body">
                     <h5 class="card-title estampa-name">{{$estampa->nome}}</h5>
                     <p class="card-text estampa-descricao">{{$estampa->descricao}}</p>
-                    <form action="{{route('cart.add')}}" method="POST">
-                        @csrf
-                        <input type="hidden" value="{{$estampa->id}}" id="estampa-id" name="estampa-id">
-                        <div class="row">
-                            {{--
-                            <span class="quantity_less" disabled="disabled">
-                                －
-                            </span>
-
-                            <label>
-                                <input class="quantity_input" data-max="100" name="quantity" type="number" value="1">
-                            </label>
-                            <span class="quantity_more">
-                                +
-                            </span>
-                        --}}
-                            <button class="btn btn-secondary btn-sm">
-                                Add to cart
-                            </button>
-                        </div>
-                    </form>
+                    @include('partials.add_to_cart', ['add_button_classes' => 'btn btn-secondary btn-sm', $estampa])
                 </div>
             </div>
         </div>
     </div>
 @endsection
+
+{{--
+<span class="quantity_less" disabled="disabled">
+    －
+</span>
+
+<label>
+    <input class="quantity_input" data-max="100" name="quantity" type="number" value="1">
+</label>
+<span class="quantity_more">
+    +
+</span>
+--}}
+
 {{--
 <div class="goodsIntro_attrBox">
 <span class="compInputNumber goodsIntro_countCtrl" id="js-goodsIntroQTY">
