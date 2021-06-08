090ddb70a76c0ecc84d067758e7ff3346e1810d6
irisamaral <2182219@my.ipleiria.pt>
2021-06-08 23:04:00 +0100

    layout update

--- resources/views/layouts/shop_layout.blade.php
+++ resources/views/layouts/shop_layout.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="refresh" content="30">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{csrf_token()}}">
@@ -11,6 +12,7 @@
     <title>{{config('app.name', 'MagicShirts')}} @yield('title')</title>
 
     <!-- Styles -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{asset('css/app.css')}}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
--- resources/views/partials/display_estampas.blade.php
+++ resources/views/partials/display_estampas.blade.php
@@ -1,10 +1,10 @@
 <div class="row estampas-area">
     @foreach($estampas as $estampa)
-        <div class="col-lg-3">
+        <div class="col-lg-3 col-sm-6 ">
             <div class="card estampa-card">
                 <a class="estampa-item-link" href="{{route('estampa.view', $estampa->id)}}">
                     <img class="card-img-top estampa-img-browse" src="{{$estampa->img}}" alt="Imagem da estampa">
-                    <div class="card-body estampa-body">
+                    <div class="card-body estampa-body text-center">
                         <h5 class="card-title estampa-name">{{$estampa->nome}}</h5>
                         <p class="card-text estampa-descricao">{{$estampa->descricao}}</p>
                         @include('partials.add_to_cart', ['add_button_classes' => 'btn btn-secondary', $estampa])
