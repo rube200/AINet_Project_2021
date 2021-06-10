9127edd0801a69c4e1d5f524f5bea05afededc2a
Rúben Garcia <rube200@users.noreply.github.com>
2021-06-10 15:58:03 +0100

    Invert style order

--- resources/views/layouts/base_template.blade.php
+++ resources/views/layouts/base_template.blade.php
@@ -12,9 +12,9 @@
     <title>{{config('app.name', 'MagicShirts')}} @yield('title')</title>
 
     <!-- Styles -->
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <link href="{{asset('css/app.css')}}" rel="stylesheet">
 </head>
 <body>
 <div>
