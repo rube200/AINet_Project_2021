9205cd53d455ce7367cfa2f45d1adfe24a37badc
Rúben Garcia <rube200@users.noreply.github.com>
2021-06-10 15:57:47 +0100

    Fix div row

--- resources/views/layouts/shop_layout.blade.php
+++ resources/views/layouts/shop_layout.blade.php
@@ -1,8 +1,6 @@
 @extends('layouts.base_template')
 @section('layout')
     <main class="pb-4">
-        <div class="row">
-            @yield('content')
-        </div>
+        @yield('content')
     </main>
 @endsection
