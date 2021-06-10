2b478a7e58f8f2f895f0a33733b4b7a37d29907b
irisamaral <2182219@my.ipleiria.pt>
2021-06-10 19:54:49 +0100

    update do layout do login e do register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.shop_layout')
 @section('title', __('Login-Title'))
 @section('content')
-    <div class="container">
+    <div class="login-container container">
         <form method="POST" action="{{route('login')}}">
             @csrf
             <label for="email">
                 {{__('Email-Input')}}
             </label>
-            <input autocomplete="email" autofocus class="form-control @error('email') is-invalid @enderror" id="email" name="email" required type="email" value="{{old('email')}}">
+            <input autocomplete="email" autofocus class="email-login form-control @error('email') is-invalid @enderror" id="email" name="email" required type="email" value="{{old('email')}}" style="width: 400px">
             @error('email')
             <strong>{{ $message }}</strong>
             @enderror
@@ -15,7 +15,7 @@
             <label for="password">
                 {{__('Password-Input')}}
             </label>
-            <input autocomplete="current-password" class="form-control @error('password') is-invalid @enderror" id="password" name="password" required type="password">
+            <input autocomplete="current-password" class="password-login form-control @error('password') is-invalid @enderror" id="password" name="password" required type="password" style="width: 400px">
             @error('password')
             <strong>{{ $message }}</strong>
             @enderror
@@ -23,17 +23,20 @@
             <label for="remember">
                 {{__('Remember-Input')}}
             </label>
-            <input {{old('remember') ? 'checked' : ''}} class="form-check-input" id="remember" name="remember" type="checkbox">
-
-            <button type="submit" class="btn btn-primary">
-                {{__('Login-Submit')}}
-            </button>
+            <input {{old('remember') ? 'checked' : ''}} class="remember-checkbox form-check-input" id="remember" name="remember" type="checkbox" style="margin-top: 10px">
 
             @if (Route::has('password.request'))
                 <a class="btn btn-link" href="{{route('password.request')}}">
                     {{__('Forget-Password-Button')}}
                 </a>
             @endif
+
+            <br>
+
+            <button type="submit" class="btn btn-primary" style="width: 400px">
+                {{__('Login-Submit')}}
+            </button>
+
         </form>
     </div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.shop_layout')
 @section('title', __('Register-Title'))
 @section('content')
-    <div class="container">
+    <div class="register-container container">
         <form method="POST" action="{{route('register')}}">
             @csrf
             <label for="name">
                 {{__('Name-Input')}}
             </label>
-            <input autocomplete="name" autofocus class="form-control @error('name') is-invalid @enderror" id="name" name="name" required type="text" value="{{old('name')}}">
+            <input autocomplete="name" autofocus class="name-register form-control @error('name') is-invalid @enderror" id="name" name="name" required type="text" value="{{old('name')}}" style="width: 400px">
             @error('name')
             <strong>{{ $message }}</strong>
             @enderror
@@ -15,7 +15,7 @@
             <label for="email">
                 {{__('Email-Input')}}
             </label>
-            <input autocomplete="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" required type="email" value="{{old('email')}}">
+            <input autocomplete="email" class="email-register form-control @error('email') is-invalid @enderror" id="email" name="email" required type="email" value="{{old('email')}}" style="width: 400px">
             @error('email')
             <strong>{{ $message }}</strong>
             @enderror
@@ -23,7 +23,7 @@
             <label for="password">
                 {{__('Password-Input')}}
             </label>
-            <input autocomplete="new-password" class="form-control @error('password') is-invalid @enderror" id="password" name="password" required type="password">
+            <input autocomplete="new-password" class="password-register form-control @error('password') is-invalid @enderror" id="password" name="password" required type="password" style="width: 400px">
             @error('password')
             <strong>{{ $message }}</strong>
             @enderror
@@ -31,9 +31,11 @@
             <label for="password-confirm">
                 {{__('Confirm-Password-Input')}}
             </label>
-            <input autocomplete="new-password" class="form-control" id="password-confirm" name="password_confirmation" required type="password">
+            <input autocomplete="new-password" class="password-confirm-register form-control" id="password-confirm" name="password_confirmation" required type="password" style="width: 400px">
 
-            <button type="submit" class="btn btn-primary">
+            <br>
+
+            <button type="submit" class="btn btn-primary" style="width: 400px">
                 {{__('Register-Submit')}}
             </button>
         </form>
