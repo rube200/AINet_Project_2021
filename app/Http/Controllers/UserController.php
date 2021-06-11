94d29ead324892988e7d373f1436a14ebc78261b
Rúben Garcia <rube200@users.noreply.github.com>
2021-06-11 20:23:54 +0100

    Block and delete profiles

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,30 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserPost;
 use App\Models\User;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\View\View;
 
 class UserController extends Controller
 {
     public function __construct()
     {
         $this->authorizeResource(User::class, null, [
-            'except' => [ 'show' ] /* for some reason this policy doesn't work */
+            'except' => [ 'destroy', 'show', 'update' ] /* for some reason theses don't work */
         ]);
     }
 
     /**
      * Display a listing of the resource.
      *
-     * @return Response
+     * @return View
      */
-    public function index()
+    public function index(): View
     {
         $users = User::select('id', 'name', 'tipo', 'bloqueado', 'foto_url')->orderBy('id')->paginate(20);
         foreach ($users as $user)
-            $this->prepareEstampaImage($user);
+            UserController::prepareEstampaImage($user);
 
         return view('profiles.profiles')->withUsers($users);
     }
@@ -54,15 +58,16 @@ class UserController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return Response
+     * @param int $id
+     * @return View
+     * @throws AuthorizationException
      */
-    public function show($id)
+    public function show(int $id): View
     {
         $target = User::findOrFail($id);
         $this->authorize('view', $target);
 
-        $this->prepareEstampaImage($target);
+        UserController::prepareEstampaImage($target);
         return view('profiles.profile')->withUser($target);
     }
 
@@ -80,33 +85,51 @@ class UserController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return Response
+     * @param UserPost $request
+     * @param int $id
+     * @return RedirectResponse
+     * @throws AuthorizationException
      */
-    public function update(Request $request, $id)
+    public function update(UserPost $request, int $id): RedirectResponse
     {
-        //
+        $target = User::findOrFail($id);
+        $this->authorize('updateBlock', $target);
+
+        $userData = $request->validated();
+
+        /* todo use with to return msg */
+        if ($userData['toggleBlock'])
+        {
+            $target->bloqueado = !$target->bloqueado;
+            $target->save();
+            return redirect()->back();
+        }
+
+        return redirect()->back();
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
-     * @return Response
+     * @param int $id
+     * @return RedirectResponse
+     * @throws AuthorizationException
      */
-    public function destroy($id)
+    public function destroy(int $id): RedirectResponse
     {
-        //
+        $target = User::findOrFail($id);
+        $this->authorize('delete', $target);
+        $target->delete();
+        return redirect()->back();
     }
 
-    public function prepareEstampaImage(User $user)
+    public static function prepareEstampaImage(User $user)
     {
         $path = 'public/fotos/' . $user->foto_url;
         if (!is_null($user->foto_url) && Storage::exists($path)) {
             $user->img = 'data:image/png;base64,' . base64_encode(Storage::get($path));
         } else {
-            $user->img = asset('storage/fotos/default-profile-picture.png');/*todo*/
+            $user->img = asset('storage/fotos/default-profile-picture.png');
         }
     }
 }
