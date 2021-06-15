bf220c7e15fd1e85c7b4a4ce1cb77842ddf56617
Rúben Garcia <rube200@users.noreply.github.com>
2021-06-15 11:59:16 +0100

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use App\Http\Controllers\CartController;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\CorController;
 use App\Http\Controllers\EstampaController;
+use App\Http\Controllers\ShopController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -18,8 +21,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Auth::routes(['register' => false, 'verify' => true]);
-Route::get('/', [EstampaController::class, 'index'])->name('index');
+Route::get('/', [ShopController::class, 'index'])->name('index');
+Route::get('shopManage', [ShopController::class, 'shopManage'])->name('shopManage')->middleware('can:isAdmin,App\Models\User');
+
 Route::resource('estampa', EstampaController::class);
+Route::resource('categoria', CategoriaController::class);
+Route::resource('cor', CorController::class);
+
 Route::resource('profile', UserController::class);
 Route::delete('profile/{user}/resetphoto', [UserController::class, 'resetPhoto'])->name('profile.photo.destroy')->middleware('can:edit,user');
 
